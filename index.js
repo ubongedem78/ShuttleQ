c0ce271b292bc227fb2133b1cab07f86138f1758
Ubong Edem <ubongedem78@gmail.com>
2023-11-08 16:08:10 +0100

    Initialize DB Connection, Setup Entry

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const { readdirSync } = require("fs");
+const { sequelize } = require("./src/config/database");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+readdirSync("./src/routes").map((path) => {
+  app.use("/api", require(`./src/routes/${path}`));
+});
+
+app.get("/", (req, res) => {
+  res.send("I AM WORKING, BUT YOUVE GOTTA WORK TOO");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`ShuttleQ is Running on Port: ${PORT}`);
+  sequelize
+    .authenticate()
+    .then(() => {
+      console.log("Database Connected");
+    })
+    .catch((err) => {
+      console.error("Database Connection Failed, error: ", err);
+    });
+});
+
+module.exports = app;
---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
+
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
+
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
+  dialect: "postgres",
+  dialectModule: require("pg"),
+});
+
+module.exports = { sequelize: sequelize };
