356d6dabccd5bf36c4423c17e0a3f1952e7a425d
Ubong Edem <ubongedem78@gmail.com>
2023-11-11 01:27:40 +0100

    Routes

--- src/routes/game.js
+++ src/routes/game.js
@@ -1,4 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const {
+  createGame,
+  startGame,
+  endGame,
+} = require("../controllers/gameController");
+
+router.post("/game/create", createGame);
+router.post("/game/start", startGame);
+router.post("/game/end", endGame);
 
 module.exports = router;
